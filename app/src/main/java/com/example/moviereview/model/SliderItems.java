bcaed0b29618fe88c1a09e6d50ac3ffb9ef05eed
Anmol Sansi <asasni@hawk.iit.edu>
2024-05-06 00:09:35 -0500

    ITMD 555 - Added OMDB Support

--- app/src/main/java/com/example/moviereview/model/SliderItems.java
+++ app/src/main/java/com/example/moviereview/model/SliderItems.java
@@ -2,21 +2,7 @@ package com.example.moviereview.model;
 
 public class SliderItems {
 
-//    private int image;
     private String posterImageUrl;
-
-//    public SliderItems(int image) {
-//        this.image = image;
-//    }
-//
-//    public int getImage() {
-//        return image;
-//    }
-//
-//    public void setImage(int image) {
-//        this.image = image;
-//    }
-
 
     public SliderItems(String posterImageUrl) {
         this.posterImageUrl = posterImageUrl;
